f6ebad45ae13142a419eb6be4156b5f3e1ca1b0b
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2025-11-04 10:11:51 +0000

    API-8734 - Migrate APE to OQE needs lib bump

--- project/LibraryDependencies.scala
+++ project/LibraryDependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object LibraryDependencies {
 
-  lazy val applicationDomainVersion = "0.88.0"
+  lazy val applicationDomainVersion = "0.90.0"
 
   def apply() = compileDependencies ++ testDependencies
 
@@ -12,9 +12,7 @@ object LibraryDependencies {
 
   lazy val testDependencies = Seq(
     "com.vladsch.flexmark"  % "flexmark-all"                             % "0.62.2",
-    "org.mockito"          %% "mockito-scala-scalatest"                  % "1.17.29",
-    "org.scalactic"        %% "scalactic"                                % "3.2.17",
-    "org.scalatest"        %% "scalatest"                                % "3.2.17",
+    "org.mockito"          %% "mockito-scala-scalatest"                  % "2.0.0",
     "uk.gov.hmrc"          %% "api-platform-application-domain-fixtures" % applicationDomainVersion
   ).map(_ % "test")
 }
